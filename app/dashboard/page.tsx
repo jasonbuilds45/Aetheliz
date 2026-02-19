352e607fb6d8007dd3a3f335c5df073f68d0681e
jasonbuilds45 <jason454a@gmail.com>
2026-02-19 16:57:43 +0530

    10

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,32 +1,11 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+const { data: profile, error } = await supabase
+  .from('profiles')
+  .select('*')
+  .eq('id', session.user.id)
+  .maybeSingle()
 
-export default async function DashboardDebug() {
-  const cookieStore = cookies()
-
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value
-        },
-        set() {},
-        remove() {},
-      },
-    }
-  )
-
-  const { data: { session } } = await supabase.auth.getSession()
-
-  return (
-    <pre>
-      {JSON.stringify(
-        { sessionExists: !!session, userId: session?.user?.id ?? null },
-        null,
-        2
-      )}
-    </pre>
-  )
-}
+return (
+  <pre>
+    {JSON.stringify({ profile, error }, null, 2)}
+  </pre>
+)
